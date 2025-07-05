06fedb20840a43223e551e8fc4fe8bdd51c5189e
mbilal-21 <bilalm3313@gmail.com>
2025-07-05 12:27:07 +0500

    first-comit

---
+++ src/app/disease-detection/page.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+const diseaseResults = [
+	{
+		name: "Aphids",
+		description:
+			"Small, pear-shaped insects that feed on plant sap, causing stunted growth and leaf curling.",
+		image:
+			"https://lh3.googleusercontent.com/aida-public/AB6AXuDre-agWP29NADAwJ2V-MiDO4tpHq7WSNFwo2yfieKGG0J-14UaMSyHVj9vf79-lZZxHVgWFGwyiJCHWyp2sjrrCqG7sER3N7zCi092tE4nbzG-5JScwUldJsS6Q51D6ybhUpByRsNUUFn9683L_Ghmq59E9aT2sdsJtvymhX6TkVYGHIbU8vHpGRoUi256vKTuwvV0gv3j3-2jLVT_sW-WryBIYfeS-QtOK-e55HyyCeArzPNFO_yvq8MsuizwUIA9GeNzwqGwzVE",
+		solution:
+			"Use insecticidal soap or neem oil to control aphid populations. Introduce natural predators like ladybugs to help manage infestations.",
+	},
+	{
+		name: "Whiteflies",
+		description:
+			"Tiny, winged insects that suck plant sap and transmit viruses, leading to yellowing and reduced yields.",
+		image:
+			"https://lh3.googleusercontent.com/aida-public/AB6AXuBRdjwgcJsPAFypeATlgBnwpKNyl2tep6dAucBU8Qk_csIbc-kDrJ5yAekYuWettxNiQT8J1guwbeYPS760X-2Aw56HMa0XV0wqXNtmN7o3hEEYBLcY41aAjmgpQFZ7rlGixpnFzjoIUsj5xSOt5L6z8TEw1fudHFM5gpY9bZr2kqpouUEbtiqQ0cp25A3KNfvkSWNfOGQu6E36mTTBHOkJuxvxJ0ZJHua_ebFs1BSaw39VzLAa1unmrRv1el4ZMD283EZ3q4fzhG4",
+		solution:
+			"Apply insecticidal soap or neem oil to control whitefly populations. Use yellow sticky traps to monitor and reduce their numbers.",
+	},
+	{
+		name: "Spider Mites",
+		description:
+			"Minute pests that create webs and feed on plant cells, causing stippling and bronzing of leaves.",
+		image:
+			"https://lh3.googleusercontent.com/aida-public/AB6AXuAmm3faZwsZK9yHFcrSinFJhH-HtKraOJB03NzM_mah5dRxJOD8Yfi-7Tn7t2RPg_C0SaKGxFVpPP4jDSWVo0wN9iCdgmnqVgDnx80nGKaf9qJNWoE6W5ZwxSn1jDo2ocTT6TUij98_Wrn6G0V_pWjtPa7cyHHpwfSWDV6fcfx9D4_q8UKLhBChXRPY3kFeWMV3a1-DSAb__Jl6lLAoOj0rYBNKHZQrER39XlD1717bjCBDkzsqgtsasGGAh8lOBl6JcrD7yhVdkt0",
+		solution:
+			"Use miticides or insecticidal soap to control spider mite populations. Increase humidity around plants to make conditions less favorable for mites.",
+	},
+];
+
+const DiseaseDetectionPage = () => {
+    return (
+        <div className="flex flex-col items-center py-8 bg-[#111714]">
+				<h2 className="text-white text-2xl font-bold mb-6">
+					Disease Detection Results
+				</h2>
+				<div className="w-full max-w-2xl space-y-6">
+					{diseaseResults.map((pest) => (
+						<div
+							className="flex flex-col md:flex-row items-center gap-4 bg-[#1a2320] rounded-lg p-4"
+							key={pest.name}
+						>
+							<div className="flex-1">
+								<p className="text-white text-lg font-bold">{pest.name}</p>
+								<p className="text-[#9eb7a8] text-sm mb-3">
+									{pest.description}
+								</p>
+								<p className="text-white text-sm">Solution:</p>
+								{/* <button className="bg-[#29382f] text-white text-sm rounded px-4 py-2 disabled:opacity-50">
+								Solutions:
+							</button> */}
+								<p className="text-[#9eb7a8] text-sm mb-3">
+									{pest.solution}
+								</p>
+							</div>
+							<div
+								className="w-full md:w-60 aspect-video bg-center bg-cover rounded"
+								style={{ backgroundImage: `url("${pest.image}")` }}
+							></div>
+						</div>
+					))}
+				</div>
+			</div>
+    );
+}
+export default DiseaseDetectionPage;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,70 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  const tabs = [
+    {
+      href: "/pest-detection",
+      label: "Pest Detection",
+    },
+    {
+      href: "/disease-detection",
+      label: "Disease Detection",
+    },
+    {
+      href: "/maturity-detection",
+      label: "Maturity Detection",
+    },
+  ];
+  return (
+    <header className="w-full bg-[#111714]">
+      <div className="flex items-center justify-between border-b border-[#29382f]  px-4 md:px-10 py-3">
+        <div className="flex items-center gap-2 text-white">
+          <div >
+            {/* <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M44 4H30.6666V17.3334H17.3334V30.6666H4V44H44V4Z" fill="currentColor"></path>
+            </svg> */}
+            <img src="./assets/logo2.png" alt="AgroAegis Logo" width="36px" height="36px"  />
+          </div>
+          <h2 className="text-lg font-bold tracking-[-0.015em] mt-2">AgroAegis</h2>
+        </div>
+
+
+      </div>
+      <div className="max-w-screen-xl mx-auto flex flex-col px-4 md:px-10 py-5">
+        <div className="flex flex-wrap justify-between gap-3 p-0 md:p-4">
+          <p className="text-white text-2xl md:text-[32px] font-bold leading-tight min-w-60 md:min-w-72">
+            Smart Agriculture 4.0 Dashboard
+          </p>
+        </div>
+        {/* -------------------------- */}
+        <div className="pb-3">
+          <div className="flex border-b border-[#3d5245] px-2 md:px-4 gap-4 md:gap-8 overflow-x-auto">
+            {tabs.map((tab) => {
+              const isActive = pathname === tab.href;
+              return (
+                <Link
+                  key={tab.href}
+                  href={tab.href}
+                  className={`flex flex-col items-center justify-center border-b-[3px] pb-3 pt-4 ${isActive
+                      ? "border-b-white text-white"
+                      : "border-b-transparent text-[#9eb7a8]"
+                    }`}
+                >
+                  <p className={`text-sm font-bold leading-normal tracking-[0.015em] ${isActive ? "text-white" : "text-[#9eb7a8]"}`}>
+                    {tab.label}
+                  </p>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
