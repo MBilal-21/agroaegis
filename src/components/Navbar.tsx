789d94938bd30161ddf1096ab0147afe7a720361
mbilal-21 <bilalm3313@gmail.com>
2025-07-06 15:07:05 +0500

    back to theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,10 +23,10 @@ export default function Navbar() {
     },
   ];
   return (
-    <header className="w-full bg-gray-50 text-black">
+    <header className="w-full bg-[#111714] ">
       {/* bg-[#111714] */}
       <div className="flex items-center justify-between border-b border-[#29382f]  px-4 md:px-10 py-3">
-        <div className="flex items-center gap-2 text-black">
+        <div className="flex items-center gap-2 text-white">
           <div >
             {/* <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M44 4H30.6666V17.3334H17.3334V30.6666H4V44H44V4Z" fill="currentColor"></path>
@@ -40,7 +40,7 @@ export default function Navbar() {
       </div>
       <div className="max-w-screen-xl mx-auto flex flex-col px-4 md:px-10 py-5">
         <div className="flex flex-wrap justify-between gap-3 p-0 md:p-4">
-          <p className="text-black text-2xl md:text-[32px] font-bold leading-tight min-w-60 md:min-w-72">
+          <p className="text-white text-2xl md:text-[32px] font-bold leading-tight min-w-60 md:min-w-72">
             Smart Agriculture 4.0 Dashboard
           </p>
         </div>
@@ -54,11 +54,11 @@ export default function Navbar() {
                   key={tab.href}
                   href={tab.href}
                   className={`flex flex-col items-center justify-center border-b-[3px] pb-3 pt-4 ${isActive
-                      ? "border-b-white text-black"
+                      ? "border-b-white text-white"
                       : "border-b-transparent text-[#9eb7a8]"
                     }`}
                 >
-                  <p className={`text-sm font-bold leading-normal tracking-[0.015em] ${isActive ? "text-black" : "text-gray-500"}`}>
+                  <p className={`text-sm font-bold leading-normal tracking-[0.015em] ${isActive ? "text-white" : "text-gray-500"}`}>
                     {/* text-[#9eb7a8] */}
                     {tab.label}
                   </p>
